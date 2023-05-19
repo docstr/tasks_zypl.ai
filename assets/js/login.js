990af0da689a9bea09c85d7ab30a97b3565d97d7
oliya_rax <62050501+docstr@users.noreply.github.com>
2023-05-20 03:33:50 +0500

    Update login.js

---
+++ assets/js/login.js
@@ -0,0 +1,4 @@
+document.querySelector("button").onclick = () => {
+    console.log(document.querySelector("#input_login").value);
+    console.log(document.querySelector("#input_password").value);
+}
